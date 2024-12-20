ff1dff62db053b5def10eb8a96389c25c8e73347
pravinthapa <prabinthapaparbin33@gmail.com>
2024-12-20 10:45:00 +0545

    screen
    screenchange

---
+++ lib/onboarding/second_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  State<SecondPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("second page"),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:prabin/onboarding/fifth_page.dart';
+import 'package:prabin/onboarding/first_page.dart';
+import 'package:prabin/onboarding/fourth_page.dart';
+import 'package:prabin/onboarding/second_page.dart';
+import 'package:prabin/onboarding/third_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,17 +13,90 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+int _selectedIndex = 0;
+
+final List _pages = [
+  FirstPage(),
+  SecondPage(),
+  ThirdPage(),
+  FourthPage(),
+  FifthPage(),
+
+  // Container(
+  //   color: Colors.red,
+  // ),
+  // Container(
+  //   color: Colors.blue,
+  // ),
+  // Container(
+  //   color: Colors.green,
+  // ),
+  // Container(
+  //   color: Colors.purple,
+  // ),
+  // Container(
+  //   color: Colors.yellow,
+  // ),
+];
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(child: Text("hello world")),
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(_selectedIndex == 0
+                  ? 'assets/vectors/home-bold.svg'
+                  : 'assets/vectors/home-outline.svg'),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(_selectedIndex == 1
+                  ? 'assets/vectors/search-bold.svg'
+                  : 'assets/vectors/search-outline.svg'),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(_selectedIndex == 2
+                  ? 'assets/vectors/add-square-bold.svg'
+                  : 'assets/vectors/add-square-outline.svg'),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(_selectedIndex == 3
+                  ? 'assets/vectors/video-play-bold.svg'
+                  : 'assets/vectors/video-play-outline.svg'),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Container(
+                height: 30,
+                width: 30,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/vectors/person.png')),
+                    shape: BoxShape.circle),
+              ),
+              label: ''),
         ],
       ),
     );
   }
+
+  // return const Scaffold(
+  //   body: Column(
+  //     mainAxisAlignment: MainAxisAlignment.center,
+  //     crossAxisAlignment: CrossAxisAlignment.center,
+  //     children: [
+  //       Center(child: Text("hello world")),
+  //     ],
+  //   ),
+  // );
 }
