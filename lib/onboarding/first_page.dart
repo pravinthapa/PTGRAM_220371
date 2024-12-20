48215999a45f832c39c68d19d499e9bc53895096
pravinthapa <prabinthapaparbin33@gmail.com>
2024-12-20 10:44:32 +0545

    onborading
    onboarding change

---
+++ lib/onboarding/first_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("first page"),
+        ],
+      ),
+    );
+  }
+}
