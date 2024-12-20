ff048d59c2b45d3b43364d4b37e221ed9aa0443d
pravinthapa <prabinthapaparbin33@gmail.com>
2024-12-20 15:27:47 +0545

    theme
    themechanged

---
+++ lib/core.theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme() {
+  return ThemeData(
+      primarySwatch: Colors.orange,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: AppBarTheme(backgroundColor: Colors.white));
+}
