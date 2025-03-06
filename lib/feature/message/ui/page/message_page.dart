26b4e1427b82224c4f1f25d00ba31301486ba329
pravinthapa <prabinthapaparbin33@gmail.com>
2025-03-06 11:49:43 +0545

    message screen completed

--- lib/feature/message/ui/page/message_page.dart
+++ lib/feature/message/ui/page/message_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ptgram/feature/auth/cubit/get_all_users_cubit.dart';
-import 'package:ptgram/feature/auth/cubit/get_user_profile_cubit.dart';
-import 'package:ptgram/feature/message/ui/widget/messgae_page_widget.dart';
+import 'package:prabin/feature/auth/cubit/get_all_users_cubit.dart';
+import 'package:prabin/feature/message/ui/widget/messgae_page_widget.dart';
 
 class MessagePage extends StatelessWidget {
   const MessagePage({super.key});
