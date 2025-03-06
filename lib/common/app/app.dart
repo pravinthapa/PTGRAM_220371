d2903b8bc1b04b0870d4f65f5797d8a90b56bf8e
pravinthapa <prabinthapaparbin33@gmail.com>
2025-03-06 11:08:50 +0545

    working

--- lib/common/app/app.dart
+++ lib/common/app/app.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:khalti_flutter/khalti_flutter.dart';
-import 'package:ptgram/common/app/env.dart';
-import 'package:ptgram/common/app/multi_repositroy.dart';
-import 'package:ptgram/common/app/multibloc_wrapper.dart';
-import 'package:ptgram/common/app/theme.dart';
-import 'package:ptgram/common/navigation/navigation_service.dart';
-import 'package:ptgram/common/route/route.dart';
-import 'package:ptgram/common/route/routing.dart';
+import 'package:prabin/common/app/multi_repositroy.dart';
+import 'package:prabin/common/app/multibloc_wrapper.dart';
+import 'package:prabin/common/app/themes.dart' as CustomTheme;
+import 'package:prabin/common/navigation/navigation_service.dart';
+import 'package:prabin/common/route/route.dart';
+import 'package:prabin/common/route/routing.dart';
 
 class App extends StatelessWidget {
   final Env env;
